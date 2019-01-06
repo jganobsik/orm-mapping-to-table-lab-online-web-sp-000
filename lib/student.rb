78b183fd03335d0a05c248a8f9a18ddc46d8230a
Joe Ganobsik <jganobsik@gmail.com>
2019-01-06 21:41:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
   def self.save
       sql = <<-SQL 
         
-       INSERT INTO students (name, grade) ; 
+       INSERT INTO students (name, grade) VALUES (?,?); 
        SQL
   DB[:conn].execute(sql)
     
