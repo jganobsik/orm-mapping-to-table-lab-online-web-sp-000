535651d759170dc48bf0ebd9fd3c2b93445af63c
Joe Ganobsik <jganobsik@gmail.com>
2019-01-06 21:34:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,8 +10,8 @@ class Student
   end
     
     
-    def self.create_table
-      sql = <<- SQL 
+    #def self.create_table
+    #  sql = <<- SQL 
         
         CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
