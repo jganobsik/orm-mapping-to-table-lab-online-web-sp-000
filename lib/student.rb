0ff9cc4ceeff1d92d3cfcf763e3468a9606e0289
Joe Ganobsik <jganobsik@gmail.com>
2019-01-06 21:33:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,8 @@ class Student
   attr_reader :id
   
   def initialize(name:, grade:, id: = nil)
-    @name = name,
-    @grade = grade, 
+    @name = name
+    @grade = grade
     @id = id 
   end
     
