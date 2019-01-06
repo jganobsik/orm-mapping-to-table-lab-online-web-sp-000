310d34491e885bf736bdb2f91c5ad90f188c90d2
Joe Ganobsik <jganobsik@gmail.com>
2019-01-06 21:37:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,4 +25,17 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  
+  self.drop_table
+  sql = <<-SQL 
+        
+       CREATE TABLE IF NOT EXISTS students (
+       id INTEGER PRIMARY KEY, 
+       name TEXT, 
+       grade INTEGER
+       )
+        
+       SQL
+  DB[:conn].execute(sql)
+    end
 end
